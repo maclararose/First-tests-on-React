84817e6ca0a950355ee82e2131365f1fea56e5d5
maclararose <maria.uespi@outlook.com>
2020-04-25 21:08:02 -0300

    Update Apps

---
+++ MeuApp/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Fabrica de Aplicativos</Text>
+      <Text>Olá Mundo</Text>
+      <Text>Testando o App</Text>
+      <Text>Meu App com Expo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
